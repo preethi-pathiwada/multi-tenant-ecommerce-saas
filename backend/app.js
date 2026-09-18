210feb8276e2882c836fea3c4c947faa5698776f
preethi-pathiwada <preethipathiwada@gmail.com>
2026-09-18 22:34:19 +0530

    Fixed CORS issue

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(helmet());
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://multi-tenant-ecommerce-saas-xi.vercel.app",
+  "https://multi-tenant-ecommerce-saas-694d03os1-npte.vercel.app/",
 ];
 
 app.use(
